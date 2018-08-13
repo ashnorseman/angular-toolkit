cdea31df4a7d7de2ea88d34bac11e39c441547d1
Ash <ash.norseman@gmail.com>
2018-08-13 15:22:04 +0800

    export pipes

--- projects/toolkit/src/public_api.ts
+++ projects/toolkit/src/public_api.ts
@@ -10,4 +10,8 @@ export * from './lib/services/http/response-error';
 
 export * from './lib/services/i18n/i18n.service';
 
+export * from './lib/pipes/sanitise/sanitise.pipe';
+export * from './lib/pipes/time/time.pipe';
+export * from './lib/pipes/value-text/value-text.pipe';
+
 export * from './lib/validators/email-validator';
