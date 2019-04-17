946e2d892e9a72a255f4dd2cc770394140af8bb3
Ash <ash.norseman@gmail.com>
2019-04-17 13:37:50 +0800

    translation tool supports project name

--- projects/toolkit/src/lib/services/i18n/i18n.service.ts
+++ projects/toolkit/src/lib/services/i18n/i18n.service.ts
@@ -1,18 +1,13 @@
 /**
  * i18n translation service
- * WARNING: Use curly quotations instead of quotation mark!!!
- * e.g. i18n.trans('“Philips Lighting” app')
  */
 
 import { Injectable } from '@angular/core';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class I18nService {
-
-  constructor() { }
 
   /**
    * Translate a string with variables
@@ -21,18 +16,21 @@ export class I18nService {
    * - 'Delete luminaire $1', 'Lum_01' -> 'Delete luminaire Lum_01'
    * - 'Delete luminaire $1 in project $2', 'Lum_01', 'Project_02' -> 'Delete luminaire Lum_01 in project Project_02'
    * - 'Delete $1 [$1|luminaire|luminaires]', 20 -> 'Delete 20 luminaires'
+   * - '{tou}Terms of use' -> 'Terms of use' (tou) is the name of project
    */
   trans(str: string, ...args: any[]): string {
     if (!str) { return ''; }
 
     if (!args.length) { return str; }
 
+    str = str.replace(/^{[^}]+}/, '');
+
     // insert variables
     args.forEach((arg: any, i: number) => {
       const index = i + 1;
 
       // Replace plural strings
-      const pluralReg = new RegExp(`\\[\\$${index}\\|(\\w+)\\|(\\w+)]`);
+      const pluralReg = new RegExp(`\\[\\$${index}\\|([^|]+)\\|([^|]+)]`);
       const pluralMatch = str.match(pluralReg);
 
       if (pluralMatch) {
